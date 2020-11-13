e6fe0d6ef76182e920998fdc59123a6a8c2a82b3
OhtakeYusuke <o.yusuke0501@gmail.com>
2020-11-13 23:59:03 +0900

    repair_all_system

--- bingo_card.rb
+++ bingo_card.rb
@@ -1,20 +1,51 @@
+
 b = (1..15).to_a.shuffle.take(5)
 i = (16..30).to_a.shuffle.take(5)
 n = (31..45).to_a.shuffle.take(5)
 g = (46..60).to_a.shuffle.take(5)
 o = (61..75).to_a.shuffle.take(5)
 
-columns = [b, i, n, g, o].transpose
+lines = [b, i, n, g, o]
+columns = lines.transpose
 columns[2][2] = nil
 
-columns.map! do |column|
-  column.map! do |number|
+pre_cards = columns.map do |pre_card|
+  pre_card.map do |number|
     number.to_s.rjust(2)
   end
 end
 
-lines = columns.map! do |line|
-  line.join("|")
+cards = pre_cards.map! do |card|
+  card.join("|")
 end
 
-puts lines
+numbers = [*1..75]
+
+selected_number = numbers.shuffle!.take(1)
+
+def check_line_number(lines,selected_number)
+  lines.map! do |number|
+    if number == selected_number
+      number = nil
+    end
+  end
+end
+
+def check_column_number(lines,selected_number)
+  columns.map! do |number|
+    if number == selected_number
+      number = nil
+    end
+  end
+end
+
+def check_cards_number(cards, selected_number)
+  cards.map! do |number|
+    if number == selected_number
+      number = nil
+    end
+  end
+end
+
+check_cards_number(cards, selected_number)
+puts cards
--- bingo_system.rb
+++ bingo_system.rb
@@ -1,3 +1,14 @@
-require "./bingo_card"
 
 
+numbers = [*1..75]
+
+selected_number = numbers.shuffle!.take(1)
+
+def check_line_number(lines,selected_number)
+  lines.map! do |number|
+    if number == selected_number
+      puts "#{number}が当たりました"
+      number = nil
+    end
+  end
+end
